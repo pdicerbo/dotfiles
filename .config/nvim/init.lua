6e87923c6a6a258193c1fd5bbd6ec27fad7f22bf
Pierluigi Di Cerbo <pierluigi.dicerbo@gmail.com>
2025-11-26 16:42:44 +0100

    kitty audio bell disabled; lazygit config improved, commit sort order set to "author-date"; tokionight colorscheme set by default, copilot cfg fixed; bare repo management by neovim improved

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -39,15 +39,28 @@ local function update_git_env_for_dotfiles()
     local home = vim.fn.expand("~")
     local git_dir = home .. "/.dotfiles-bare-repo" -- Replace .dotfiles.git with your bare repo name
 
-    -- Check if the current directory is within the scope you want to manage with the bare repo
-    -- For dotfiles, this is usually the home directory
-    if vim.fn.getcwd() == home then
-        vim.env.GIT_WORK_TREE = home
-        vim.env.GIT_DIR = git_dir
+    -- If GIT_DIR is already set to something, do nothing
+    if vim.env.GIT_DIR ~= nil and vim.env.GIT_DIR ~= git_dir then
+        return
+    end
+
+    -- check if the current working directory should belong to dotfiles
+    local cwd = vim.loop.cwd()
+    local git_folder = cwd .. "/.git"
+    local has_local_git = vim.loop.fs_stat(git_folder) ~= nil
+
+    if (vim.startswith(cwd, home .. "/.config/") or cwd == home) and not has_local_git then
+        if vim.env.GIT_DIR == nil then
+            -- export git location into ENV
+            vim.env.GIT_DIR = git_dir
+            vim.env.GIT_WORK_TREE = home
+        end
     else
-        -- Clear the variables if not in the bare repo's scope, so other repos work normally
-        vim.env.GIT_WORK_TREE = nil
-        vim.env.GIT_DIR = nil
+        if vim.env.GIT_DIR == git_dir then
+            -- unset variables
+            vim.env.GIT_DIR = nil
+            vim.env.GIT_WORK_TREE = nil
+        end
     end
 end
 
